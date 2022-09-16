06373e8b750a7b10529fec25737655cb33526246
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-09-16 16:58:45 +0100

    Working on Git Config In Dev

---
+++ .github/workflows/Utilities/Utils-Synapse-Dev-Git-Config.sh
@@ -0,0 +1,8 @@
+az synapse workspace update \
+                --name "synapsewsdeploychd"
+                --resource-group "synapse-dev-rg"
+                --account-name "ciaran28" \
+                --collaboration-branch "main" \
+                --repository-type "GitHub" \
+                --repository-name "SynapseArtifacts" \
+                --root-folder "/synapseresources" \
