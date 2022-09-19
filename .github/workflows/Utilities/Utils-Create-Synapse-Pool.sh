4ef7626a1fd82aa237900c40c7c659dfc80cc63c
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-09-19 16:29:00 +0100

    Testing new pool creation

---
+++ .github/workflows/Utilities/Utils-Create-Synapse-Pool.sh
@@ -0,0 +1,44 @@
+
+LIST_CLUSTERS=$(az synapse spark pool list \
+                --resource-group "$bicep_parameters_resourceGroupName_value" \
+                --workspace-name "$bicep_parameters_workspaceName_value")
+echo "List Clusters: $LIST_CLUSTERS"
+
+# Extract Existing Cluster Names
+CLUSTER_NAMES=$( jq -r '[.[].name]' <<< "$LIST_CLUSTERS")
+echo "Cluster Names: $CLUSTER_NAMES"
+
+echo "Ingest JSON Environment File"
+JSON=$( jq '.' .github/workflows/Pipeline_Param/$environment.json)
+#echo "${JSON}" | jq
+
+
+echo "Configure All Synapse Spark Pools.... "
+for row in $(echo "${JSON}" | jq -r '.Clusters[] | @base64'); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+    if [[ ! " ${CLUSTER_NAMES[*]} " =~ "$(_jq '.cluster_name')" ]]; then
+
+        echo "Synapse Pool Does Not Exist: Create Synapse Pool... "
+        
+        az synapse spark pool create \
+            --name "$(_jq '.cluster_name')" \
+            --node-size "$(_jq '.node_size')" \
+            --node-size-family "$(_jq '.node_size_family')" \
+            --spark-version "$(_jq '.spark_version')" \
+            --node-count "$(_jq '.node_count')" \
+            --enable-auto-pause "$(_jq '.enable_auto_pause')" \
+            --delay "$(_jq '.auto_pause_delay_in_minutes')" \
+            --enable-auto-scale "$(_jq '.enable_auto_scale')" \
+            --max-node-count "$(_jq '.autoscale_max_node_count')" \
+            --min-node-count "$(_jq '.autoscale_min_node_count')" \
+            --resource-group "$bicep_parameters_resourceGroupName_value" \
+            --workspace-name "$bicep_parameters_workspaceName_value" \
+            --no-wait
+
+    else
+        echo "Cluster Exists... Ignore"  
+    fi
+done
+
