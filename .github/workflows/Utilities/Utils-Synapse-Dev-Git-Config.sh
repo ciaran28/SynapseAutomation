b5411dc0c15f6a1d73d2c76748256b63c92d2c9c
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-09-19 13:55:20 +0100

    Testing Parameter Ingestion

--- .github/workflows/Utilities/Utils-Synapse-Dev-Git-Config.sh
+++ .github/workflows/Utilities/Utils-Synapse-Dev-Git-Config.sh
@@ -1,8 +1,8 @@
 az synapse workspace update \
-                --name "synapsewsdeploychd" \
-                --resource-group "synapse-dev-rg" \
-                --account-name "ciaran28" \
-                --collaboration-branch "main" \
-                --repository-type "GitHub" \
-                --repository-name "SynapseArtifacts" \
-                --root-folder "/synapseresources" 
+                --name "$bicep_parameters_workspaceName_value" \
+                --resource-group "$bicep_parameters_resourceGroupName_value" \
+                --account-name "$pipeline_Git_Configuration_0_git_username" \
+                --collaboration-branch "$pipeline_Git_Configuration_0_branch" \
+                --repository-type "$pipeline_Git_Configuration_0_git_provider" \
+                --repository-name "$pipeline_Git_Configuration_0_repository_name" \
+                --root-folder "$pipeline_Git_Configuration_0_repo_root_folder"
